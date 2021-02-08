6e5bc962b5de29db04c7f0732ba59d5b7322d7ec
aceberry924 <meobius123@gmail.com>
2021-02-08 16:12:12 +0800

    add register page

---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+//import { AuthService } from 'src/app/_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    // private auth: AuthService,
+    public translate: TranslateService,
+  ) { }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
+  }
+
+}
