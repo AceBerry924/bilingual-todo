39486c78d9cc4944804a9229897350c1c71fbed1
aceberry924 <meobius123@gmail.com>
2021-02-11 17:44:23 +0800

    add firebaseConfig to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCjak-Pc0RNkvxEfrxq918v9QTczB3on3s',
+    authDomain: 'bilingual-todo.firebaseapp.com',
+    databaseURL: 'https://bilingual-todo.firebaseapp.com',
+    projectId: 'bilingual-todo',
+    storageBucket: 'bilingual-todo.appspot.com',
+    messagingSenderId: '241748469060',
+    appId: '1:241748469060:web:feff653c8c081c8296e311',
+    measurementId: 'G-MFTPZKB379'
+  }
 };
